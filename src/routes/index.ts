26d979b25065d45263177e1d79f9349788b8206c
MaxSavostyanov <maxsavostyanov34@gmail.com>
2023-11-26 22:36:11 +0300

    feat: создайте роут для логина и регистрации

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
 import usersRouter from './users';
 import cardsRouter from './cards';
+import login from '../controllers/login';
+import { createUser } from '../controllers/users';
 
 const router = Router();
+
+router.post('/signin', login);
+router.post('/signup', createUser);
 
 router.use('/users', usersRouter);
 router.use('/cards', cardsRouter);
