8903437c15fca81bf03c76d28aa7e5c17985e5ef
MaxSavostyanov <maxsavostyanov34@gmail.com>
2023-11-26 23:25:29 +0300

    feat: сделайте мидлвар для авторизации

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,11 +3,14 @@ import usersRouter from './users';
 import cardsRouter from './cards';
 import login from '../controllers/login';
 import { createUser } from '../controllers/users';
+import auth from '../middlewares/auth';
 
 const router = Router();
 
 router.post('/signin', login);
 router.post('/signup', createUser);
+
+router.use(auth);
 
 router.use('/users', usersRouter);
 router.use('/cards', cardsRouter);
