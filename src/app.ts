4302f42761b832ee2b39c7ea2a663d017d4efbc9
MaxSavostyanov <maxsavostyanov34@gmail.com>
2023-11-27 18:51:21 +0300

    feat: защитите API авторизацией

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
 import router from './routes';
 import errorHandler from './middlewares/error-handler';
 import NotFoundError from './errors/not-found-error';
@@ -9,6 +10,8 @@ const { PORT = 3000 } = process.env;
 const app = express();
 app.use(express.json());
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+
+app.use(cookieParser());
 
 app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
   req.user = {
