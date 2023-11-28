0f9fad1f69c04309d765ec9135cd2eaa605c1c55
MaxSavostyanov <maxsavostyanov34@gmail.com>
2023-11-28 16:48:33 +0300

    feat: проверка данных поля avatar

--- src/models/user.ts
+++ src/models/user.ts
@@ -37,6 +37,12 @@ const UserSchema = new Schema<IUser>({
     type: String,
     required: false,
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    validate: {
+      validator(url: string) {
+        return validator.isURL(url);
+      },
+      message: 'Некорректный Email',
+    },
   },
   email: {
     type: String,
